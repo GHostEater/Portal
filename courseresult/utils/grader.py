a4d1129d511103e9128b25bd03884f13c343e679
GHostEater <lasisi28@gmail.com>
2018-11-12 10:48:38 +0100

    Abstract Grading to rely on Database Tables

--- courseresult/utils/grader.py
+++ courseresult/utils/grader.py
@@ -19,30 +19,6 @@ def grader(total):
             gp = grade.gp
             break
 
-    # if (total >= 70.00) and (total <= 100.00):
-    #     grade = 'A'
-    #     gp = 5
-    # elif (total >= 60.00) and (total <= 69.00):
-    #     grade = 'B'
-    #     gp = 4
-    # elif (total >= 50.00) and (total <= 59.00):
-    #     grade = 'C'
-    #     gp = 3
-    # elif (total >= 45.00) and (total <= 49.00):
-    #     grade = 'D'
-    #     gp = 2
-    # elif (total >= 40.00) and (total <= 44.00):
-    #     grade = 'E'
-    #     gp = 1
-    # elif total <= 39.00:
-    #     grade = 'F'
-    #     gp = 0
-    #
-    # if grade == 'F':
-    #     status = 0
-    # else:
-    #     status = 1
-
     data = {
         "grade": g,
         'gp': gp,
