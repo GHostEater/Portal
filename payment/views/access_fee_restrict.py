d55028b132b39bab8737e005a42c9d9683817323
GHostEater <lasisi28@gmail.com>
2018-11-24 07:14:51 +0100

    Access Fee Corrections

--- payment/views/access_fee_restrict.py
+++ payment/views/access_fee_restrict.py
@@ -5,7 +5,6 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
-from accounts.models import Student
 from payment.models import Payment
 from paymentwaving.models import WavedPayment
 
@@ -15,19 +14,17 @@ from paymentwaving.models import WavedPayment
 def access_fee_restrict(request):
     req = request.GET
 
-    student = Student.objects.get(pk=req['student'])
-
     paid = False
 
     try:
-        Payment.objects.get(student=req['student'], payment_type__name="Portal Access Fee", level=student.level,
+        Payment.objects.get(student=req['student'], payment_type__name="Portal Access Fee",
                             paid=True)
         paid = True
     except Payment.DoesNotExist:
         paid = False
 
     try:
-        WavedPayment.objects.get(student=req['student'], payment_type__name="Portal Access Fee", level=student.level)
+        WavedPayment.objects.get(student=req['student'], payment_type__name="Portal Access Fee")
         paid = True
     except WavedPayment.DoesNotExist:
         if paid:
