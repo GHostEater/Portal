0ee313bb1fceb8adb3386259afb149db25a5566a
GHostEater <lasisi28@gmail.com>
2018-12-07 11:23:11 +0100

    Payment HotFixes
    (cherry picked from commit d9f317c9aa3973389e1afff1a8a4e10ddd7af7ab)
    
    # Conflicts:
    #	Portal.zip
    #	payment/views/generate_rrr.py

--- payment/views/generate_rrr.py
+++ payment/views/generate_rrr.py
@@ -8,9 +8,12 @@ import requests
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from accounts.models import Student
 from level.models import Level
 from payment.models import Payment
 from payment.serializers import PaymentSerializer
+from paymenttype.models import PaymentType
+from session.models import Session
 
 
 @api_view(['POST'])
@@ -20,16 +23,30 @@ def generate_rrr(request):
 
     matric_no = ''
     dept = ''
-    level = Level()
 
-    if hasattr(req, 'matricNo'):
+    if request.POST.get('matricNo'):
         matric_no = req['matricNo']
-    if hasattr(req, 'dept'):
+    if request.POST.get('dept'):
         dept = req['dept']
-    if hasattr(req, 'level'):
+    if request.POST.get('level'):
         level = Level.objects.get(pk=req['level'])
+    else:
+        level = Level()
+        level.level = ''
 
-    payment = Payment.objects.get(pk=req['payment'])
+    student = Student.objects.get(pk=req['student'])
+    payment_type = PaymentType.objects.get(pk=req['payment_type'])
+    session = Session.objects.get(pk=req['session'])
+
+    payment = Payment()
+    payment.payment_type = payment_type
+    payment.student = student
+    payment.session = session
+    if level.level != '':
+        payment.level = level
+    payment.order_id = req['order_id']
+    payment.status = req['status']
+    payment.date = req['date']
 
     hsh = hashlib.sha512()
     hsh.update(
@@ -51,26 +68,27 @@ def generate_rrr(request):
             "type": "ALL"
         },
             {
-            "name": "Department",
-            "value": dept,
-            "type": "ALL"
-        },
+                "name": "Department",
+                "value": dept,
+                "type": "ALL"
+            },
             {
-            "name": "Level",
-            "value": level.level,
-            "type": "ALL"
-        },
+                "name": "Level",
+                "value": level.level,
+                "type": "ALL"
+            },
             {
-            "name": "Purpose of Payment",
-            "value": payment.payment_type.name,
-            "type": "ALL"
-        }
+                "name": "Purpose of Payment",
+                "value": payment.payment_type.name,
+                "type": "ALL"
+            }
         ]
     }
 
-    header = {"Authorization": "remitaConsumerKey="+payment.payment_type.merchant_id+",remitaConsumerToken="+hsh_dig,
-              "Content-Type": "application/json"
-            }
+    header = {
+        "Authorization": "remitaConsumerKey=" + payment.payment_type.merchant_id + ",remitaConsumerToken=" + hsh_dig,
+        "Content-Type": "application/json"
+        }
 
     r = requests.post(url=gen_rrr, headers=header, json=request)
     print r.text
