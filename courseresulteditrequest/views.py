df85f2b0fa41a521f09112769001bc56f99b5c73
GHostEater <lasisi28@gmail.com>
2019-02-27 09:26:28 +0100

    - Request edit privileges send notification email to dean - Time restriction on edit privileges - Payment reports show s/n, RRR in table - Payment reports for all students in the school, all in a college, all in a level but entire school or college - Payment reports total amount owing in a report - Payment of fines with custom amounts (tag=fine) - Added start date in session - Bug fixes
    (cherry picked from commit d63d9b2)

--- courseresulteditrequest/views.py
+++ courseresulteditrequest/views.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
+import datetime
+from mailqueue.models import MailerMessage
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.generics import CreateAPIView, RetrieveUpdateDestroyAPIView, ListAPIView
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
+from accounts.models import Lecturer, Dean
 from courseresulteditrequest.serializers import LogSerializer, RequestSerializer, LogCreateSerializer, RequestCreateSerializer
 from courseresulteditrequest.models import Log, Request
 
@@ -63,3 +69,36 @@ class LogDetailAPIView(RetrieveUpdateDestroyAPIView):
         queryset = Log.objects.all()
         queryset = self.get_serializer_class().setup_eager_loading(queryset)
         return queryset
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def notify_dean(request):
+    req = request.GET
+    lecturer = Lecturer.objects.get(pk=req['lecturer'])
+    dean = Dean.objects.get(college=lecturer.dept.college)
+
+    subject = req['school_name'] + " - Lecturer Applied For Edit Privileges"
+
+    message = MailerMessage()
+    message.subject = subject
+    message.to_address = dean.user.username
+    message.from_address = req['email']
+    message.content = lecturer.user.last_name+", "+lecturer.user.first_name+' applied for edit privileges, check and ' \
+                                                                            'review application '
+    message.app = req['school_name'] + " Mailing System"
+    message.save()
+    return Response({})
+
+
+@api_view(['GET'])
+def auto_disable_edit(request):
+    requests = Request.objects.all()
+    for req in requests:
+        try:
+            date_compare = req.end_date <= datetime.datetime.now()
+        except:
+            continue
+        if date_compare:
+            req.delete()
+    return Response({})
